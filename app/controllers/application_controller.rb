fbc1dc32cd8a3605816921c17be6a88f9c3692b0
hiro-nishino <126562444+hiro-nishino@users.noreply.github.com>
2023-04-13 09:53:36 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :encrypted_password ])
   end
 end
